66e8a08a686b86641ea5cdeb5b624c1e14c88689
Samir1984 <allandanielito1984@gmail.com>
2026-07-16 18:29:06 -0600

    Creación de clase Libros con su estructura e implementacion de metodos

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,33 @@
+use std::mem::transmute_copy;
+
 fn main() {
+    struct Libros{
+        autor:String,
+        titulo:String,
+        disponible:bool,
+    }
+
+    impl Libros{
+        pub fn constructor(titulo:String, autor:String)->Libros{
+            Libros { autor, titulo, disponible: true }
+        }
+        pub fn esta_disponible(&self) -> bool {
+        self.disponible
+    }
+        pub fn prestar(&mut self)->bool{
+            if self.disponible{
+                self.disponible=false;
+                true
+            }
+            else {
+                false
+            }
+        }
+        pub fn devolver(&mut self){
+            self.disponible=true;
+        }
+
     
+    }
 
 }
