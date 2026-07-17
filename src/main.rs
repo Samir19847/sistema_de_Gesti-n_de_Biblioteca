8b48f04aa5562abec538f63086dee3f0cf6e439d
Samir1984 <allandanielito1984@gmail.com>
2026-07-16 18:57:15 -0600

    Implementacion de contratos de comportamiento para libros

--- src/main.rs
+++ src/main.rs
@@ -29,5 +29,41 @@ fn main() {
 
     
     }
+    trait Prestable {
+        fn titulo(&self) -> String;
+        fn estadisponible(&self) -> bool;
+        fn prestar (&mut self) -> bool;
+        fn devolver(&mut self);
+        
+    }
+    impl Prestable for Libros{
+        fn titulo (&self)->String{
+            self.titulo.clone()
+        }
+        fn estadisponible(&self) -> bool {
+            self.disponible
+        }
+        fn prestar (&mut self) -> bool {
+            if self.disponible {
+            self.disponible = false;
+            true
+        } else {
+            false
+        }
+        }
+        fn devolver(&mut self) {
+            self.disponible=true;
+        }
 
-}
+
+
+
+
+
+
+
+
+        }
+    }
+
+
